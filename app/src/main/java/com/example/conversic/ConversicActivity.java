11d44d677b63074073b163ca7fdca8978d63530f
tzexern <tantzexern@gmail.com>
2020-05-29 23:42:42 +0800

    UI for conversic, crash course, library, etc.

---
+++ app/src/main/java/com/example/conversic/ConversicActivity.java
@@ -0,0 +1,14 @@
+package com.example.conversic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConversicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversic);
+    }
+}
