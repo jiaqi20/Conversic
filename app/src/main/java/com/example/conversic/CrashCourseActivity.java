177c204496bdc12116a9c60956628d4423ec0af7
tzexern <tantzexern@gmail.com>
2020-06-01 23:21:07 +0800

    UI for conversic, crash course, library, etc. Save 1.5 CrashCourse File lmao

---
+++ app/src/main/java/com/example/conversic/CrashCourseActivity.java
@@ -0,0 +1,35 @@
+package com.example.conversic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CrashCourseActivity extends AppCompatActivity {
+
+    //Navigation: Back Button
+    private Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crash_course);
+
+        //Navigation: Back Button
+        btnBack = findViewById(R.id.buttonBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveBack();
+            }
+        });
+    }
+
+    private void moveBack(){
+        Intent intent = new Intent(CrashCourseActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
